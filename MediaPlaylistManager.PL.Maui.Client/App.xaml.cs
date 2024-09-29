32a438b66b07dd00962ddd486553a880723aeaae
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-09-29 05:42:05 +0200

    set minimum window size for Windows and MacCatalyst
    Added an override for CreateWindow to set a minimum height of 800 and width of 700 for Windows and MacCatalyst platforms. This ensures a consistent window size across different operating systems, enhancing user experience.

--- MediaPlaylistManager.PL.Maui.Client/App.xaml.cs
+++ MediaPlaylistManager.PL.Maui.Client/App.xaml.cs
@@ -5,10 +5,25 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        
+
         if (Current != null)
             Current.UserAppTheme = AppTheme.Light;
 
         MainPage = new AppShell();
+    }
+
+    // Source:
+    // https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/windows?view=net-maui-8.0
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+#if WINDOWS || MACCATALYST
+        window.MinimumHeight = 800d;
+        window.MinimumWidth = 700d;
+#endif
+
+        return window;
     }
 }
